6a55add6ce3b493eae5c592a872721cd51b183ef
Bharath1910 <av.bharath@proton.me>
2022-12-29 21:18:27 +0530

    fixed minor things

--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -5,7 +5,13 @@ databaseUrl = process.env.DB_URL
 
 async function connect() {
     try {
-        await mongoose.connect(databaseUrl)
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(databaseUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: 'Todo'
+        })
+        console.log("Connected!")
     }
 
     catch (error) {
