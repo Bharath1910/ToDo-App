49498a7dcb9e7b8ad832807c332d9070546dff01
Bharath1910 <av.bharath@proton.me>
2022-12-30 15:19:42 +0530

    testing backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Add from './Add';
-import List from './List'
+import List from './List';
+import Sync from './Sync'
 
 function App() {
     const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('/api/test')
+            .then(responce => responce.json())
+            .then(data => console.log(data))
+    }, [])
+
 
     function handleToggle(uid) {
         const dataCopy = [...data]
@@ -17,6 +25,7 @@ function App() {
         <>
             <Add data={data} setData={setData}/>
             <List data={data} handleToggle={handleToggle}/>
+            <Sync/>
         </>
   );
 }
