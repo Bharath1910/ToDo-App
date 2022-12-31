03e4b45b863d99aea748df82cefc8520d7b9048c
Bharath1910 <av.bharath@proton.me>
2022-12-31 09:43:23 +0530

    completed sync component, sync works well

--- client/src/components/Sync.js
+++ client/src/components/Sync.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import cookies from 'js-cookie'
 
 function Sync(data) {
     async function uploadData() {
+        const token = cookies.get('token')
+        const postData = {...data, token: token}
+        console.log(postData)
         const raw = await fetch('http://localhost:5500/api/uploadData', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(postData)
         })
 
         const content = await raw.json();
